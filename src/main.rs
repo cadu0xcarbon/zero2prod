1828da10ae6c7844beacbf0fffe78973e2663fc0
cadu <cadu@0xcarbon.org>
2023-03-13 15:21:36 -0300

    health_check setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
